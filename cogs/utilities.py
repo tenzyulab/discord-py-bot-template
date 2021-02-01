9a06719728ee76034f32c68e65c365172aabff2c
tenzyu <tenzyu.on@gmail.com>
2021-02-01 13:40:04 +0900

    added ping command

---
+++ cogs/utilities.py
@@ -0,0 +1,20 @@
+from time import monotonic
+
+from discord.ext.commands import Bot, Cog, Context, command
+
+
+class Utilities(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @command()
+    async def ping(self, ctx: Context):
+        """check the connection and response time"""
+        tmp = monotonic()
+        msg = await ctx.send("Calculating...")
+        latency = (monotonic() - tmp) * 1000
+        await msg.edit(content=f"Pong! Response time is **{int(latency)}** ms.")
+
+
+def setup(bot: Bot):
+    bot.add_cog(Utilities(bot))
