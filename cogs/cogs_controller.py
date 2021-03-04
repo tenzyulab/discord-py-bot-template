6b75bbb8f89bb0ecab8b56417fb2c5771071d852
tenzyu <tenzyu.on@gmail.com>
2021-03-04 22:41:33 +0900

    improved cogs_controller

--- cogs/cogs_controller.py
+++ cogs/cogs_controller.py
@@ -8,23 +8,23 @@ class CogsController(Cog, command_attrs=dict(hidden=True)):
     async def cog_check(self, ctx: Context):
         if await ctx.bot.is_owner(ctx.author):
             return True
-        await ctx.send("You cannot run this command.")
+        await ctx.reply("You cannot run this command.")
         return False
 
     @command(name="load")
     async def load_cog(self, ctx: Context, cog: str):
-        self.bot.load_extension("cogs." + cog)
-        await ctx.send(f"Loaded Extension: {cog}.py")
+        self.bot.load_extension(f"cogs.{cog}")
+        await ctx.reply(f"Loaded Extension: {cog}")
 
     @command(name="unload")
     async def unload_cog(self, ctx: Context, cog: str):
-        self.bot.unload_extension("cogs." + cog)
-        await ctx.send(f"Unloaded Extension: {cog}.py")
+        self.bot.unload_extension(f"cogs.{cog}")
+        await ctx.reply(f"Unloaded Extension: {cog}")
 
     @command(name="reload")
     async def reload_cog(self, ctx: Context, cog: str):
-        self.bot.reload_extension("cogs." + cog)
-        await ctx.send(f"Reloaded Extension: {cog}.py")
+        self.bot.reload_extension(f"cogs.{cog}")
+        await ctx.reply(f"Reloaded Extension: {cog}")
 
 
 def setup(bot: Bot):
