4706d69943ca4c09cd2ee6f35ad0198df0be4338
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-06-24 10:59:03 +0900

    refactor command_error_handler.py

--- cogs/command_error_handler.py
+++ cogs/command_error_handler.py
@@ -1,24 +1,19 @@
-from discord.ext.commands import (
-    Bot,
-    CheckFailure,
-    Cog,
-    CommandError,
-    CommandNotFound,
-    Context,
-)
+from discord.ext.commands import Bot, CheckFailure, Cog, CommandError, CommandNotFound, Context
 
 
-class ErrorHandler(Cog):
+class CommandErrorHandler(Cog):
+    __slots__ = "bot"
+
     def __init__(self, bot: Bot):
         self.bot = bot
 
     @Cog.listener()
     async def on_command_error(self, ctx: Context, error: CommandError):
-        ignore_errors = (CommandNotFound, CheckFailure)
+        ignore_errors = (CheckFailure, CommandNotFound)
         if isinstance(error, ignore_errors):
             return
         await ctx.reply(error)
 
 
 def setup(bot: Bot):
-    bot.add_cog(ErrorHandler(bot))
+    bot.add_cog(CommandErrorHandler(bot))
